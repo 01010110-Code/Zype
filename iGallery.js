8ba84cba9f2c13c6eb544d940cd8a8ffd4a4e5a0
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-03-02 20:18:09 +0100

    removed the interractive gallery

--- iGallery.js
+++
@@ -1,8 +0,0 @@
-const galleryContainer = document.querySelector('.gallery-container');
-const thumbnails = document.querySelectorAll('.thumbnails img');
-
-thumbnails.forEach(thumbnail => {
-  thumbnail.addEventListener('click', () => {
-    galleryContainer.src = thumbnail.src.replace('-thumb', '');
-  });
-});
